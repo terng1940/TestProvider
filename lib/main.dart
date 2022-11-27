81acdfa14832fc4b54e72e89eab4fee96a664ba2
terng1940 <teng1940@gmail.com>
2022-11-28 02:49:08 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/form_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Accounting app'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+          actions: [
+            IconButton(
+                onPressed: (() {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return FormScreen();
+                  }));
+                }),
+                icon: const Icon(Icons.add))
+          ],
+        ),
+        body: Container());
+  }
+}
---
+++ lib/screens/form_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class FormScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Record Form"),
+        ),
+        body: Form(
+          child: Column(
+            children: [
+              
+            ],
+          ),
+        )
+        );
+  }
+}
